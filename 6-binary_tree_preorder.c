7494de9a0afd36f37ec1c810249ac10acd161c3d
root <jadsika@gmail.com>
2024-02-29 23:39:13 +0000

    done for now

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_preorder - goes through a inary tree using preorder traversal
+ *@treee: pointer to the root of the node of the tree to traverse
+ */
+
+void binaty_tree_preorder(const binary_tree_t *tree,void (*func)(int))
+{
+  if (tree && func)
+    {
+      func(tree->n);
+
+      binary_tree_preorder(tree->left, func);
+      binary_tree_preorder(tree->right, func);
+    }
+}
